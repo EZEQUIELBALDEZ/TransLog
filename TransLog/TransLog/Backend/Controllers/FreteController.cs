5a08188aaa4c62237ed202339a9613ab446c0663
Ezequiel Sousa Baldez <Senai@CEPEAS21104014>
2024-03-28 21:42:53 -0300

    Commit

--- TransLog/TransLog/Backend/Controllers/FreteController.cs
+++ TransLog/TransLog/Backend/Controllers/FreteController.cs
@@ -2,6 +2,8 @@
 
 using Backend.Models.Request;
 using Backend.Models.Response;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Backend.Controllers
 {
@@ -11,22 +13,28 @@ namespace Backend.Controllers
 
     public class FreteController : ControllerBase
     {
-
+        decimal valorQuilometro = 0.50m;
+        decimal adicionalQuilo = 1.00m;
+        decimal adicionaloCubico = 2.00m;
         [HttpGet]
         [Route("obter")]
-        public IActionResult ObterCarga ([FromQuery] CargaRequest request)
+
+        public IActionResult Obter ([FromQuery] CargaRequest request)
         {
 
-            //Se o pessoa nasceu antes de 1920, a data de nascimento está errada teoricamente ninguem
-            //vivo nasceu antes de 1920
-
-            decimal valorQuilometro = 0.50m;
-            decimal adicionalQuilo = 1.00m;
-            decimal adicionaloCubico = 2.00m;
-
-            var ValorFrete;
+            //   A fórmula para calcular o custo do frete é a seguinte: Custo do Frete = (Distância × Valor Fixo por
+            //Km) +(Peso × Valor Adicional por Kg) +(Largura × Altura × Valor Adicional por m3)
 
 
+
+
+
+            var ValorFrete = (Distancia.request * valorQuilometro) + (Peso.request * adicionalQuilo) + (Largura.request * Altura.request * adicionaloCubico);
+
+
+
+            var response = new cargaResponse();
+            response.ValorDotransporte = ValorFrete;
 
 
             return Ok(response);
